ff78c2260d60f6f2a2343ec37a4635ec01eb2ba9
Montanah <mititi.isaac@gmail.com>
2024-03-02 21:00:17 +0300

    Minor js functions edits

--- location-tracker/src/components/Sidebar.js
+++ location-tracker/src/components/Sidebar.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProfileDisplay from './ProfileDisplay';
 
 const Sidebar = () => {
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setHidden(true);
+      } else {
+        setHidden(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
     
     return (
-      <div className="sidebar">
-        <div className="sidebarItem">
-          <span className="sidebarTitle">Profile</span>
-          < ProfileDisplay />
-        </div>
+      <div className={`sidebar ${hidden ? 'hidden' : ''}`}>
+        <div className="toggleButton" onClick={() => setHidden(!hidden)}>
+        {hidden ? 'Show Sidebar' : 'Hide Sidebar'}
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">Profile</span>
+        <ProfileDisplay />
+      </div>
         <div className="sidebarItem">
           <ul className="sidebarList">
             <li className="sidebarListItem">Locations</li>
