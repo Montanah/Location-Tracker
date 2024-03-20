3403202e5d8ec70a44039d5bdc0b2f802e62e4df
Montanah <mititi.isaac@gmail.com>
2024-03-20 12:57:48 +0300

    Switching from MySQL DB to MongoDB: adding the dbConnection.js and minor app.js refactorization

--- app.js
+++ app.js
@@ -6,31 +6,17 @@ const CORS = require('cors');
 const app = express();
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 8000;
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const { MongoClient } = require('mongodb');
+const connectToMongoDB = require('./dbConnection');
 dotenv.config({ path: "./.env"})
 
 //connecting the db
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD, 
-    database: process.env.DATABASE
+connectToMongoDB().then((db) => {
+    app.locals.db = db;
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
 });
-
-// Handling connection errors
-db.on('error', (err) => {
-    console.error('MySQL connection error:', err);
-  });
-  
-  // Establishing the database connection
-  db.connect((err) => {
-    if (err) {
-      console.error('Error connecting to MySQL:', err);
-      return;
-    }
-    console.log('Connected to MySQL database');
-  });
 
 //using the hbs template engine
 //this gives access to the current directory
@@ -38,6 +24,10 @@ const publicDirectory = path.join(__dirname, './public');
 app.use(CORS());
 app.use(express.static(publicDirectory));
 app.set('view engine', 'hbs');
+
+// Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 //importing the routes
 const countries = require('./routes/countries');
@@ -47,10 +37,6 @@ const profile = require('./routes/profile');
 const addLocations = require('./routes/locations');
 const displayLocations = require('./routes/locations');
 
-// Middleware to parse JSON bodies
-app.use(express.urlencoded({ extended: false}));
-app.use(express.json());
-
 //defined routes
 app.use('/countries', countries);
 app.use('/', require('./routes/users'))
@@ -58,6 +44,22 @@ app.use('/auth', require('./routes/auth'));
 app.use('/profile', require('./routes/profile'));
 app.use('/addLocation', addLocations);
 app.use('/displayLocations', displayLocations);
+
+// Home route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// 404 route
+app.use((req, res) => {
+    res.status(404).send('404: Page not found');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 // Start the server
 app.listen(port, host, () => {
